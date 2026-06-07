e5c2d5cacf8a008712d4d22bd432a72d85f796a7
Tim Mahoney <timmahoney2000@gmail.com>
2026-06-06 23:51:02 -0400

    changed dummy API call to actual API call

--- nj-food-pantries-api/nj-food-pantries-api/src/server.js
+++ nj-food-pantries-api/nj-food-pantries-api/src/server.js
@@ -17,7 +17,10 @@ const allowedOrigins = process.env.FRONTEND_ORIGIN
 ? [process.env.FRONTEND_ORIGIN]
 : ["http://localhost:5500", "http://127.0.0.1:5500",
  "http://localhost:3000", "http://127.0.0.1:3000",
- "http://localhost:8080", "http://127.0.0.1:8080",];
+ "http://localhost:8080", "http://127.0.0.1:8080",
+  "https://neighborhoodnourish.vercel.app",
+  "https://neighborhoodnourish-8k0df1rdb-tims-projects-43918398.vercel.app",
+];
 
 app.use(cors({
   origin: (origin, callback) => {
