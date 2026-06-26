479293ba7033c347011b65fb5c52458105a6d566
Tim Mahoney <timmahoney2000@gmail.com>
2026-06-26 19:52:57 -0400

    resolve CORS error

--- src/server.js
+++ src/server.js
@@ -18,8 +18,8 @@ const allowedOrigins = process.env.FRONTEND_ORIGIN
 : ["http://localhost:5500", "http://127.0.0.1:5500",
  "http://localhost:3000", "http://127.0.0.1:3000",
  "http://localhost:8080", "http://127.0.0.1:8080",
-origin === "https://neighborhoodnourish.org" ||
-origin === "https://www.neighborhoodnourish.org" ||
+  "https://neighborhoodnourish.org",
+  "https://www.neighborhoodnourish.org",
   "https://neighborhoodnourish.vercel.app",
   "https://neighborhoodnourish-8k0df1rdb-tims-projects-43918398.vercel.app",
   "https://neighborhoodnourish.org",
